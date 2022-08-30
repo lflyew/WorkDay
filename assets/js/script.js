51900e5efe615be9b48aef97e7f51fac6ead80ed
Latoya Lyew <nursebae@Latoyas-MacBook-Pro.local>
2022-08-29 21:11:05 -0400

    color coding for past present future

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,25 @@
 var day = moment().format ("MMMM Do YYYY, h:mm a");
 $("#currentDay").text(day);
 
+function hrcolor() {
+    var currenthr = moment().hour();
+    $(".time-block").each(function () {
+        var hour = parseInt($(this).attr("id").split("hour")[1]);
+        // console.log(hour);
+ 
+        if (hour < currenthr) {
+            $(this).removeClass("future present");
+            $(this).addClass("past");
+        } else if (hour === currenthr){
+            $(this).removeClass("future past");
+            $(this).addClass("present");
+        } else {
+            $(this).removeClass("present past");
+            $(this).addClass("future");
+        }
+
+        }
+    )
+    }
+
+    hrcolor();
